6ab1ec1d7a7cc577fef375e73f440fb295388d8d
Alberto <albertoboschinicr@gmail.com>
2022-01-25 13:31:40 -0600

    PROTECCION ARCHIVOS

--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-)8om-&($2cfsi+i%f7d@!jxy*$t^%z-ri#g(5rt&*bo$**01!c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
