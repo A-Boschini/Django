857969bbcb2fcbfc0a4bab56c69bd1a4df27ed28
Alberto <albertoboschinicr@gmail.com>
2022-02-01 17:23:53 -0600

    Create

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
 from django.views.generic import View
 from .forms import PostCreateForm
 
@@ -7,8 +6,7 @@ from .forms import PostCreateForm
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
         context={
-            
-        }
+          }
         return render(request, 'blog_list.html', context)
     
     class BlogCreateView(View):
@@ -17,7 +15,7 @@ class BlogListView(View):
             }
             return render(request, 'blog_create.html', context)
         
-          def post(self, request,*args, **kwargs):
-            context={
-            }
-            return render(request, 'blog_create.html', context)    
+        def post(self, request, *args, **kwargs):
+           context={
+           }
+           return render(request, 'blog_create.html', context)    
