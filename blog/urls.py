7a2c30979154925bac37b51ccd4ab22616163cf3
Alberto <albertoboschinicr@gmail.com>
2022-02-07 14:39:05 -0600

    TAILWIND-FIN DE CURSO

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('',BlogListView.as_view(),name="home"),
     path('create/',BlogCreateView.as_view(),name="create"),
     path('<int:pk>/', BlogDetailView.as_view(), name="detail"),
-    path('<int:pk>/delete'),
+    path('<int:pk>/delete', BlogDeleteView.as_view(),name='delete'),
 ]
